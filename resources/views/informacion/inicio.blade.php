e32cc77db11395e8d85d44cdb4bf4ec97048d322
PG210 <pedrocuasquer21095@gmail.com>
2021-10-12 03:20:04 -0500

    carrucel

--- resources/views/informacion/inicio.blade.php
+++ resources/views/informacion/inicio.blade.php
@@ -1,6 +1,34 @@
 @extends('welcome')
 @section('content')
 
+<!--Carroucel-->
+<div class="container">
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100 h-100" src="{{url('img/a1.jpg')}}" alt="Foto1">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100 h-100" src="{{url('img/a2.jpg')}}" alt="Foto2">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+</div>
+<!--end -->
+
 <br>
 <h3 class="text-center">SERVICIOS</h3>
 <div class="container-fluid text-center">
